5e5e6569412473b03573922281ce444d6d01e2d0
gamp300 <49374048+gamp300@users.noreply.github.com>
2023-11-02 21:17:34 -0500

    Full 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const containerGallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    element =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+      <img
+        class="gallery__image"
+        src="${element.preview}"
+        alt="${element.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+
+containerGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+containerGallery.addEventListener('click', event => event.preventDefault());
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
